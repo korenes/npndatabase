926d315f51e9eaa06bcc339ef38670bc648999a3
Korene Stuart <korene17@gmail.com>
2019-02-06 15:21:05 -0500

    initial commit

---
+++ src/main/java/com/Korene/npndatabase/NpnDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.Korene.npndatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpnDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpnDatabaseApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/Korene/npndatabase/NpnDatabaseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.Korene.npndatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NpnDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
