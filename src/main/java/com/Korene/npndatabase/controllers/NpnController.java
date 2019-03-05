6998c3314d3fb2d34573439907c7e0c387626076
Korene Stuart <korene17@gmail.com>
2019-03-05 13:35:21 -0500

    Cities and providers working on home page

--- src/main/java/com/Korene/npndatabase/controllers/NpnController.java
+++ src/main/java/com/Korene/npndatabase/controllers/NpnController.java
@@ -1,7 +1,10 @@
 package com.Korene.npndatabase.controllers;
 
 import com.Korene.npndatabase.models.Agency;
+import com.Korene.npndatabase.models.City;
 import com.Korene.npndatabase.models.data.AgencyDao;
+import com.Korene.npndatabase.models.data.CityDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("npndatabase")
 public class NpnController {
+
+    @Autowired
     private AgencyDao agencyDao;
 
-    @RequestMapping(value="")
+    @Autowired
+    private CityDao cityDao;
+
+
+    @RequestMapping(value={"", "/home"})
     public String index(Model model){
 //        model.addAttribute("title", "home");
+        Iterable<City> cities = cityDao.findAll();
+        model.addAttribute("cities", cities);
         Iterable<Agency> agencies = agencyDao.findAll();
-        System.out.println(agencies);
         model.addAttribute("agencies", agencies);
 
         return "index";
+
     }
 
     @RequestMapping(value = "contact")
@@ -29,15 +39,27 @@ public class NpnController {
         return "category/contact";
     }
 
+    @RequestMapping(value = "login")
+    public String login(Model model) {
+        model.addAttribute("title");
+
+        return "category/login";
+    }
+
     @RequestMapping(value = "about")
     public String about(Model model){
         model.addAttribute("title");
         return "category/about";
     }
 
-    @RequestMapping(value = "home")
-    public String home(){
-        return "index";
+    @RequestMapping(value = "add")
+    public String add(Model model) {
+        model.addAttribute("title");
+        return "category/add";
+
     }
+
+
+
 }
 
